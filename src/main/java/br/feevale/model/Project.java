836c0705dd544f85780b125ddfadfe585b748170
Douglas <dnevescarlos@gmail.com>
2018-04-20 19:00:23 -0300

    criando modelo project

---
+++ src/main/java/br/feevale/model/Project.java
@@ -0,0 +1,17 @@
+package br.feevale.model;
+
+public class Project extends Model {
+
+    private String description;
+
+    public Project(){
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
